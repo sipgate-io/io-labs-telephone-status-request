0dc64ee03a37a4ef99a7fd3e0c2fe994e69a6ed6
sehmer <sehmer@sipgate.de>
2021-12-16 13:34:18 +0100

    :sparkles: play announcement based on OrderStatus
    Co-authored-by: Maximilian Klingelhoeffer <klingelhoeffer@sipgate.de>

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,13 @@ dotenv.config();
 
 const MAX_CUSTOMERID_DTMF_INPUT_LENGTH = 8;
 
+enum OrderStatus {
+  RECEIVED,
+  PENDING,
+  FULLFILLED,
+  CANCELED,
+}
+
 if (!process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS) {
   console.error(
     "ERROR: You need to set a server address to receive webhook events!\n",
@@ -22,6 +29,26 @@ if (!process.env.SIPGATE_WEBHOOK_SERVER_PORT) {
 const SERVER_ADDRESS = process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS;
 const PORT = process.env.SIPGATE_WEBHOOK_SERVER_PORT;
 
+const getAnnouncementByOrderStatus = (orderStatus: OrderStatus): string => {
+  switch (orderStatus) {
+    case OrderStatus.RECEIVED:
+      return "https://github.com/sipgate-io/io-labs-telephone-status-request/blob/main/static/orderstatus_received.wav?raw=true";
+    case OrderStatus.PENDING:
+      return "https://github.com/sipgate-io/io-labs-telephone-status-request/blob/main/static/orderstatus_pending.wav?raw=true";
+    case OrderStatus.FULLFILLED:
+      return "https://github.com/sipgate-io/io-labs-telephone-status-request/blob/main/static/orderstatus_fullfilled.wav?raw=true";
+    case OrderStatus.CANCELED:
+      return "https://github.com/sipgate-io/io-labs-telephone-status-request/blob/main/static/orderstatus_canceled.wav?raw=true";
+    default:
+      return "https://github.com/sipgate-io/io-labs-telephone-status-request/blob/main/static/error.wav?raw=true";
+  }
+};
+
+const getAnnouncementByCustomerId = (customerId: string): string => {
+  console.log(`getAnnouncementByCustomerId ${customerId}`);
+  return getAnnouncementByOrderStatus(OrderStatus.PENDING);
+};
+
 createWebhookModule()
   .createServer({
     port: PORT,
@@ -33,9 +60,8 @@ createWebhookModule()
       return WebhookResponse.gatherDTMF({
         maxDigits: MAX_CUSTOMERID_DTMF_INPUT_LENGTH,
         timeout: 5000,
-        // needs to be changed
         announcement:
-          "https://github.com/sipgate-io/io-labs-complex-ivr/blob/main/static/welcome.wav?raw=true",
+          "https://github.com/sipgate-io/io-labs-telephone-status-request/blob/main/static/request_customerid.wav?raw=true",
       });
     });
 
@@ -43,12 +69,11 @@ createWebhookModule()
       const customerId = dataEvent.dtmf;
       if (customerId.length === MAX_CUSTOMERID_DTMF_INPUT_LENGTH) {
         console.log(`The caller provided a valid customer id: ${customerId} `);
+
         return WebhookResponse.gatherDTMF({
           maxDigits: 1,
           timeout: 0,
-          // needs to be changed
-          announcement:
-            "https://github.com/sipgate-io/io-labs-complex-ivr/blob/main/static/request.wav?raw=true",
+          announcement: getAnnouncementByCustomerId(customerId),
         });
       }
       return WebhookResponse.hangUpCall();
