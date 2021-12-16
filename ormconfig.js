2fbfa0216077e09585f338c599f1c31eaf2aeb6f
Max Klingelhoeffer <klingelhoeffer@sipgate.de>
2021-12-16 14:25:56 +0100

    :sparkles: add typeorm and configure it for mysql
    Co-authored-by: Malte Sehmer <sehmer@sipgate.de>

---
+++ ormconfig.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config();
+
+const {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_NAME,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+} = process.env;
+
+module.exports = {
+  type: "mysql",
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  database: DATABASE_NAME,
+  synchronize: true,
+  entities: ["src/entities/**/*.ts"],
+  migrations: ["src/migrations/**/*.ts"],
+};
